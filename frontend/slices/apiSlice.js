34aec7d171eabdb38993df0aa8b5eec9d7cfe332
shethj79-collab <shethj79@gmail.com>
2026-02-19 22:23:30 +0530

    Delete frontend/slices directory

--- frontend/slices/apiSlice.js
+++
@@ -1,9 +0,0 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-import { BASE_URL } from '../constants';
-
-export const apiSlice = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: BASE_URL }),
-  tagTypes: ['User', 'Product', 'Order'],
-  endpoints: builder => ({})
-});
-                                                              
